8e2b334649bcede65f4a314c86ea24a0be488b43
mabolek <mathias@lilio.com>
2019-03-29 13:57:53 +0100

    [BUGFIX] Added missing argument in mkdir()

--- bin/platform-404.php
+++ bin/platform-404.php
@@ -1,12 +1,12 @@
 <?php
 
-define('PLATFORM_404_FILE_PATH', 'web/typo3temp/assets/platform-404/');
+define('PLATFORM_404_FILE_PATH', 'web/typo3temp/assets/platform-404');
 
-$createdDirectory = mkdir(PLATFORM_404_FILE_PATH, true);
+$createdDirectory = mkdir(PLATFORM_404_FILE_PATH, 0777, true);
 
 if (!$createdDirectory || !file_exists(PLATFORM_404_FILE_PATH)) {
     echo 'ERROR: Could not create 404 file directory "' . PLATFORM_404_FILE_PATH . '"' . PHP_EOL;
     die(1);
 }
 
-file_put_contents(PLATFORM_404_FILE_PATH . '404.html', '<!--#echo var="HTTP_HOST"-->');
+file_put_contents(PLATFORM_404_FILE_PATH . '/' . '404.html', '<!--#echo var="HTTP_HOST"-->');
