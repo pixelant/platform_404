7c45e6cd1dc6217cfeda9821d2ac036833d44b98
mabolek <mathias@lilio.com>
2019-03-29 14:13:53 +0100

    [BUGFIX] Fixed 404 folder permissions

--- bin/platform-404.php
+++ bin/platform-404.php
@@ -2,11 +2,13 @@
 
 define('PLATFORM_404_FILE_PATH', 'web/typo3temp/assets/platform-404');
 
-$createdDirectory = mkdir(PLATFORM_404_FILE_PATH, 0777, true);
+$createdDirectory = mkdir(PLATFORM_404_FILE_PATH, 02775, true);
 
 if (!$createdDirectory || !file_exists(PLATFORM_404_FILE_PATH)) {
     echo 'ERROR: Could not create 404 file directory "' . PLATFORM_404_FILE_PATH . '"' . PHP_EOL;
     die(1);
 }
+
+chmod(PLATFORM_404_FILE_PATH, 02775);
 
 file_put_contents(PLATFORM_404_FILE_PATH . '/' . '404.html', '<!--#echo var="HTTP_HOST"-->');
